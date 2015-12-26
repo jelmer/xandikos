273c576a3ed8dabe60043300606309eea963c90c
Jelmer Vernooij <jelmer@jelmer.uk>
2015-12-26 14:26:02 +0000

    Add setup.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+version = "0.0.1"
+
+setup(name="dystros",
+      description="VCS fastimport/fastexport parser",
+      version=version,
+      author="Jelmer Vernooij",
+      author_email="jelmer@jelmer.uk",
+      license="Apache v2 or later",
+      url="https://www.jelmer.uk/projects/dystros",
+      packages=['dystros'])
