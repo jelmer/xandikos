6f4ce9289992b8961909fb27d0ce7527b929a54f
Jelmer Vernooĳ <jelmer@jelmer.uk>
2017-11-07 21:15:42 +0000

    Allow overriding litmus URL.

--- compat/litmus.sh
+++ compat/litmus.sh
@@ -9,6 +9,7 @@ if [ -n "$TESTS" ]; then
 fi
 SRCPATH="$(dirname $(readlink -m $0))"
 VERSION=0.13
+LITMUS_URL="${LITMUS_URL:-http://www.webdav.org/neon/litmus/litmus-${VERSION}.tar.gz}"
 
 scratch=$(mktemp -d)
 function finish() {
@@ -20,7 +21,7 @@ pushd "${scratch}"
 if [ -f "${SRCPATH}/litmus-${VERSION}.tar.gz" ]; then
 	cp "${SRCPATH}/litmus-${VERSION}.tar.gz" .
 else
-	wget -O "litmus-${VERSION}.tar.gz" http://www.webdav.org/neon/litmus/litmus-${VERSION}.tar.gz
+	wget -O "litmus-${VERSION}.tar.gz" "${LITMUS_URL}"
 fi
 sha256sum ${SRCPATH}/litmus-${VERSION}.tar.gz.sha256sum
 tar xvfz litmus-${VERSION}.tar.gz
