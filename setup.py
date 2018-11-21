02d8f2aacafe63cce30f14e21dd279ef3995f0c9
Daniel M. Capella <polycitizen@gmail.com>
2018-11-21 13:59:23 -0500

    setup.py: Remove unused import & add Py3.7 classifier

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@
 # MA  02110-1301, USA.
 
 from setuptools import find_packages, setup
-import sys
 
 version = "0.0.12"
 
@@ -52,6 +51,7 @@ setup(name="xandikos",
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: Implementation :: CPython',
           'Programming Language :: Python :: Implementation :: PyPy',
           'Operating System :: POSIX',
