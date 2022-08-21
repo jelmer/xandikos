74d27709bfc92e7a6362cbc7d5d71e674432175b
Jelmer Vernooĳ <jelmer@jelmer.uk>
2022-08-21 13:09:30 +0100

    Add typing.

--- xandikos/store/git.py
+++ xandikos/store/git.py
@@ -27,6 +27,7 @@ import logging
 import os
 import shutil
 import stat
+from typing import Dict, Tuple
 import uuid
 
 from . import (
@@ -224,10 +225,10 @@ class GitStore(Store):
         self.ref = ref
         self.repo = repo
         # Maps uids to (sha, fname)
-        self._uid_to_fname = {}
+        self._uid_to_fname: Dict[str, Tuple[bytes, str]]  = {}
         self._check_for_duplicate_uids = check_for_duplicate_uids
         # Set of blob ids that have already been scanned
-        self._fname_to_uid = {}
+        self._fname_to_uid: Dict[str, Tuple[str, str]] = {}
 
     @property
     def config(self):
